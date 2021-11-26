1fd19ea0c23fc61437ae1dbd5303a7f2728ce041
Dow Core <935693215@qq.com>
2021-11-26 18:01:10 +0800

    init

--- src/permission.js
+++ src/permission.js
@@ -4,14 +4,12 @@ import { Message } from 'element-ui'
 import { useNProgress } from '@vueuse/integrations/useNProgress'
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
-
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
-
 const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
 
+const  progress = useNProgress()
 router.beforeEach(async (to, from, next) => {
   // start progress bar
-  NProgress.start()
+  progress.value = true
 
   // set page title
   document.title = getPageTitle(to.meta.title)
@@ -23,7 +21,7 @@ router.beforeEach(async (to, from, next) => {
     if (to.path === '/login') {
       // if is logged in, redirect to the home page
       next({ path: '/' })
-      NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
+      progress.value = false // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
     } else {
       // determine whether the user has obtained his permission roles through getInfo
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
@@ -49,7 +47,7 @@ router.beforeEach(async (to, from, next) => {
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
           next(`/login?redirect=${to.path}`)
-          NProgress.done()
+          progress.value = false
         }
       }
     }
--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: 'Vue Meta',
 
   /**
    * @type {boolean} true | false
